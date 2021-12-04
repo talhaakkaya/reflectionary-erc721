c18464199b5c0e7e39b8ada930442efed16abd55
Talha Akkaya <talha2akkaya@gmail.com>
2021-12-04 10:58:29 -0500

    deploy script updated

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,17 +2,22 @@ import { ethers } from "hardhat";
 
 async function main() {
   const [owner] = await ethers.getSigners();
+  const tokenName = "Token Name";
+  const tokenSymbol = "TOKENSYMBOL";
+  const uri = "ipfs://uri/";
+  const reflectionShare = 30;
+  const tokenPrice = ethers.utils.parseUnits("1", 18);
 
   const ReflectionaryERC721 = await ethers.getContractFactory(
     "ReflectionaryERC721"
   );
   const master = await ReflectionaryERC721.deploy(
-    "Token Name",
-    "TOKENSYM",
+    tokenName,
+    tokenSymbol,
     await owner.getAddress(),
-    "ipfs://uri/",
-    30,
-    "1000000000000000000"
+    uri,
+    reflectionShare,
+    tokenPrice
   );
 
   await master.deployed();
